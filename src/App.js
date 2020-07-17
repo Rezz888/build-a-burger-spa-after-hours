7caf81fc5ca8c9a81ab8a91030c31491c8125a60
Vincent Sanders <vincesanders@hotmail.com>
2020-07-16 21:28:43 -0500

    Added components we think we will need.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+//NavBar
+//HomePage
+//BurgerForm
 
 function App() {
   return (
     <div>
+      <NavBar />
+      <Switch>
+        <Route path='/buildaburger'>
+          <BurgerForm />
+        </Route>
+        <Route exact path='/'>
+          <HomePage />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/BurgerForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BurgerForm = props => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default BurgerForm;
