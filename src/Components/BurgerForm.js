e8cfbe8a4cfb00f9a19b7f313dc342f395960ce8
Rezaur Rahman <rezaur.rahman_nyc@icloud.com>
2020-07-26 13:33:10 -0400

    Some progress made: up to 2 hours of the video

--- src/Components/BurgerForm.js
+++ src/Components/BurgerForm.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import * as yup from 'yup';
-import styled from "styled-component";
+import styled from "styled-components";
 
 const defaultFormState = {
     name: "",
@@ -28,10 +28,33 @@ const defaultErrorState = {
 const BurgerForm = props => {
     const [formState, setFormState] = useState(defaultFormState);
     const [errors, setErrors] = useState(defaultErrorState);
-    const [isDisabled, setIsDisabled] = useState(false);
+    const [isDisabled, setIsDisabled] = useState(true);
+
+    const handleChange = e => {
+        if (e.target.type === 'checkbox') {
+            setFormState({
+                ...formState,
+                 condiments: {
+                    ...formState.condiments,
+                [e.target.value]: e.target.checked
+                }
+            })
+        } else {
+            setFormState({
+                ...formState,
+                 [e.target.name]: e.target.value
+            })
+        }
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        console.log(formState)
+    }
 
     return (
-        <div>
+        <FormContainer>
+            <form onSubmit={handleSubmit}>
             <label>
                 Name
                 <input type='text' name='name' onChange={handleChange} value={formState.name} data-cy='name' />
@@ -55,51 +78,72 @@ const BurgerForm = props => {
             
             <fieldset>
                 <label>
-                    <input type='' name='' onChange={handleChange} value={} data-cy='' />
+                    <input type='radio' name='patties' onChange={handleChange} value={1} data-cy='1' />
                     1 patty
                 </label>
                 <label>
-                    <input type='' name='' onChange={handleChange} value={} data-cy='' />
+                    <input type='radio' name='patties' onChange={handleChange} value={2} data-cy='2' />
                     2 patties
                 </label>
                 <label>
-                    <input type='' name='' onChange={handleChange} value={} data-cy='' />
+                    <input type='radio' name='patties' onChange={handleChange} value={3} data-cy='3' />
+                    3 patties
                 </label>
             </fieldset>
 
             <fieldset>
                 <label>
-                    <input type='' name='' onChange={handleChange} value={} data-cy='' />
+                    <input type='checkbox' name='condiments' onChange={handleChange} value="ketchup" data-cy='ketchup' />
+                    Ketchup
                 </label>
                 <label>
-                    <input type='' name='' onChange={handleChange} value={} data-cy='' />
+                    <input type='checkbox' name='condiments' onChange={handleChange} value="mustard" data-cy='mustard' />
+                    Mustard
                 </label>
                 <label>
-                    <input type='' name='' onChange={handleChange} value={} data-cy='' />
+                    <input type='checkbox' name='condiments' onChange={handleChange} value="mayonaise" data-cy='mayonaise' />
+                    Mayonaise
                 </label>
                 <label>
-                    <input type='' name='' onChange={handleChange} value={} data-cy='' />
+                    <input type='checkbox' name='condiments' onChange={handleChange} value="barbecue" data-cy='barbecue' />
+                    Berbecue
                 </label>
                 <label>
-                    <input type='' name='' onChange={handleChange} value={} data-cy='' />
+                    <input type='checkbox' name='condiments' onChange={handleChange} value="onion" data-cy='onion' />
+                    Onion
                 </label>
                 <label>
-                    <input type='' name='' onChange={handleChange} value={} data-cy='' />
+                    <input type='checkbox' name='condiments' onChange={handleChange} value="lettuce"
+                     data-cy='lettuce' />
+                     Lettuce
                 </label>
                 <label>
-                    <input type='' name='' onChange={handleChange} value={} data-cy='' />
+                    <input type='checkbox' name='condiments' onChange={handleChange} value="cheese" data-cy='cheese' />
+                    Cheese
+
                 </label>
                 <label>
-                    <input type='' name='' onChange={handleChange} value={} data-cy='' />
+                    <input type='checkbox' name='condiments' onChange={handleChange} value="bacon" data-cy='bacon' />
+                    Bacon
                 </label>
             </fieldset>
             <label>
-                <textarea />
+                <textarea name="instructions" data-cy="instructions" onChange={handleChange} value={formState.instructions}/>
             </label>
 
-            <button>Order Your Burger</button>
-        </div>
+            <button type="submit" disabled={false}>Order Your Burger</button>
+            </form>
+        </FormContainer>
     );
 
 }
+
+const FormContainer = styled.div`
+
+      margin: 5rem auto;
+      width: 900px;
+      display: flex;
+      flex-direction: column;
+      color: #fff;
+`
 export default BurgerForm;
